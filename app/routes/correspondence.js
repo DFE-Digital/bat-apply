1208f41f71dec624f208b2cc6673844663e8b582
Fazal Khan <fazal.khan@digital.education.gov.uk>
2018-11-22 12:46:41 +0000

    complete correspondence flow

--- app/routes/correspondence.js
+++ app/routes/correspondence.js
@@ -1,18 +1,18 @@
 const correspondenceRouter = (req, res) => {
     // main/correspondence
-    const area = req.session.data['where-do-you-live'];
+    const area = req.session.data['correspondence-where-do-you-live'];
     const correspondence = req.session.data['correspondence_address'];
     
-    if (correspondence === 'yes') {
-        if (area === 'uk') {
+    if (area) {
+        if (area === 'correspondence_uk') {
             res.redirect('/contact-details/correspondence-uk');
-        } else if (area === 'international') {
+        } else if (area === 'correspondence_international') {
             res.redirect('/contact-details/correspondence-international');
         } else {
             res.redirect('/contact-details/correspondence-bfpo');
         }
     } else {
-        res.redirect('/your-application');
+        res.render('contact-details/correspondence');
     }
 
   };
