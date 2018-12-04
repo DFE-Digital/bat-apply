db7c602547a8cd39474d083fe56607939dc0850b
Fazal Khan <fazal.khan@digital.education.gov.uk>
2018-12-04 14:42:20 +0000

    added checkbox for no qual

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,6 @@ const contactDetailsRouter = require("./routes/contact-details.js");
 const correspondenceRouter = require("./routes/correspondence.js");
 const dashboardRouter = require("./routes/dashboard.js");
 const personalDetailsRouter = require("./routes/personal-details.js");
-const educationOverviewRouter = require("./routes/education-overview.js");
 const educationRouter = require("./routes/education-and-qualifications.js");
 // -------------------------------------------------------------- routes
 router.get("/your-application", dashboardRouter);
@@ -14,7 +13,6 @@ router.get("/register/:step", registerRouter);
 router.get("/personal-details/:step", personalDetailsRouter);
 router.get("/contact-details/main/:area?", contactDetailsRouter);
 router.get("/contact-details/correspondence/:area?", correspondenceRouter);
-router.get("/education-and-qualifications/overview/:step?", educationOverviewRouter);
 router.get("/education-and-qualifications/:step?", educationRouter);
 // -------------------------------------------------------------- export
 module.exports = router;
